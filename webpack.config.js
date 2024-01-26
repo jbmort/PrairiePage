6cea2fea15d911910f53cd2d0fea9fa9b691c686
Jacob Mortensen <141834498+jbmort@users.noreply.github.com>
2024-01-26 07:29:21 -0600

    update html template

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   plugins: [new HtmlWebpackPlugin({
     title: 'Prairie Project Services',
-    template: './src/index.html',
+    template: './src/src.html',
   })],
   module: {
     rules: [
